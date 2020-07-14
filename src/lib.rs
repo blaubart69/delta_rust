635c48ea088861686c591f278d31cffb0cd9d854
bee <bernhard.spindler.75@gmail.com>
2020-07-14 06:51:58 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+use std::cmp::Ordering;
+
+pub fn run<'a, A:'a, B:'a, T1, T2, K1, K2, K3, A1, C1, C2, C3, C4>(
+    list_a              : &mut T1,
+    list_b              : &mut T2,
+    key_comparer_a_b    : K1,
+    key_comparer_a_a    : K2,
+    key_comparer_b_b    : K3,
+    attribute_comparer  : A1,
+    only_exist_in_a     : &mut C1,
+    only_exist_in_b     : &mut C2,
+    modified            : &mut C3,
+    samesame            : &mut C4,
+)
+where
+ T1 : Iterator<Item=&'a A>
+,T2 : Iterator<Item=&'a B>
+,K1 : Fn(&A, &B) -> Ordering
+,K2 : Fn(&A, &A) -> Ordering
+,K3 : Fn(&B, &B) -> Ordering
+,A1 : Fn(&A, &B) -> bool
+,C1 : FnMut(&A)
+,C2 : FnMut(&B)
+,C3 : FnMut(&A,&B)
+,C4 : FnMut(&A,&B)
+
+{
+    let mut last_a: Option<&A> = None;
+    let mut last_b: Option<&B> = None;
+
+    let mut a_opt = list_a.next();
+    let mut b_opt = list_b.next();
+
+    loop {
+        if a_opt.is_none() && b_opt.is_none() {
+            break;
+        }
+
+        if a_opt.is_some() && b_opt.is_some() {
+            let a = a_opt.unwrap();
+            let b = b_opt.unwrap();
+
+            match key_comparer_a_b(&a, &b) {
+                Ordering::Equal => {
+                    if attribute_comparer(&a, &b) == true {
+                        samesame(&a, &b);
+                    } else {
+                        modified(&a, &b);
+                    }
+                    last_a = a_opt;
+                    last_b = b_opt;
+                    a_opt = list_a.next();
+                    b_opt = list_b.next();
+                }
+                Ordering::Less => {
+                    only_exist_in_a(&a);
+                    last_a = a_opt;
+                    a_opt = list_a.next();
+                }
+                Ordering::Greater => {
+                    only_exist_in_b(&b);
+                    last_b = b_opt;
+                    b_opt = list_b.next();
+                }
+            }
+        } else if a_opt.is_some() && b_opt.is_none() {
+            only_exist_in_a(&a_opt.unwrap());
+            last_a = a_opt;
+            a_opt = list_a.next();
+        } else if a_opt.is_none() && b_opt.is_some() {
+            only_exist_in_b(&b_opt.unwrap());
+            last_b = b_opt;
+            b_opt = list_b.next();
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::run;
+    use std::cmp::Ordering;
+
+    #[test]
+    fn three_same_elemts() {
+        let l1 = vec![1,2,3];
+        let l2 = vec![1,2,3];
+
+        let mut samesame = 0;
+        let mut modified = 0;
+        let mut onlya = 0;
+        let mut onlyb = 0;
+
+        run(&mut l1.iter(),&mut l2.iter(),
+             |&a, &b| { if a < b { Ordering::Less} else if a == b { Ordering::Equal} else { Ordering::Greater} },
+             |&a1, &a2| { Ordering::Greater },
+             |&b1, &b2| { Ordering::Greater },
+             |&a, &b| { true },
+             &mut |&a| { onlya+=1; },
+             &mut |&b| { onlyb+=1; },
+             &mut |&a, &b| { modified+=1},
+             &mut |&a, &b| { samesame+=1; });
+
+        assert_eq!(3,samesame);
+        assert_eq!(0,onlya);
+        assert_eq!(0,onlyb);
+        assert_eq!(0,modified);
+    }
+    #[test]
+    fn one_element_but_different() {
+        let l1 = vec![1];
+        let l2 = vec![2];
+
+        let mut samesame = 0;
+        let mut modified = 0;
+        let mut onlya = 0;
+        let mut onlyb = 0;
+
+        run(&mut l1.iter(),&mut l2.iter(),
+            |&a, &b| { if a < b { Ordering::Less} else if a == b { Ordering::Equal} else { Ordering::Greater} },
+            |&a1, &a2| { Ordering::Greater },
+            |&b1, &b2| { Ordering::Greater },
+            |&a, &b| { true },
+            &mut |&a| { onlya+=1; assert_eq!(1, a); },
+            &mut |&b| { onlyb+=1; assert_eq!(2, b); },
+            &mut |&a, &b| { modified+=1},
+            &mut |&a, &b| { samesame+=1; });
+
+        assert_eq!(0,samesame);
+        assert_eq!(1,onlya);
+        assert_eq!(1,onlyb);
+        assert_eq!(0,modified);
+    }
+
+}
